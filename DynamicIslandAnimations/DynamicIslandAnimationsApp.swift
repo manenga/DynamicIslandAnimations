69ce80deed1dcf552e2ad728425f719440f546d4
Manenga Mungandi <manengam@rain.co.za>
2023-08-19 18:46:56 +0200

    Initial Commit

---
+++ DynamicIslandAnimations/DynamicIslandAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicIslandAnimationsApp.swift
+//  DynamicIslandAnimations
+//
+//  Created by Manenga Mungandi on 2023/08/19.
+//
+
+import SwiftUI
+
+@main
+struct DynamicIslandAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
